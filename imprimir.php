d172a4f84bca04003d758c226390cbc0e6d7ab8e
Guadalupe22 <lupitavasquez1995@hotmail.com>
2015-03-27 23:30:18 +0100

    Guia de php con base de datos

---
+++ imprimir.php
@@ -0,0 +1,37 @@
+<doctype!>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+
+    <h1>direccion de casa</h1>
+
+
+
+<?php
+
+
+$referencia = "tercera calle poniente.";
+$col = " Colonia Santa Maria. ";
+$NCasa = "Casa: numero."; $N = 39;
+$Munici = "Municipio de: San Marcos.";
+$depto = "Departamento de  San Salvador.";
+
+//acontinuacion se imprimira las variables establecidas
+
+print "$referencia <br><br>";
+print "$col <br><br>";
+print "$NCasa <br>";
+print ($N);
+print "<br>$Munici <br> <br>";
+print($depto);
+
+//imprimir
+
+
+ ?>
+ 
+  </body>
+</html>
